99c561f2b2adf0c2406c348ff226c067823a2d0d
Setteducati Michael (BDK7DQS) <BDK7DQS@ups.com>
2017-06-29 19:15:35 -0400

    Bootstrap changes on login screen.

--- lmdp/lmdp-client/src/app/app.component.ts
+++ lmdp/lmdp-client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Login{
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  title = 'The Last Mile Package Delivery';
+  login: Login = {
+    email: null,
+    password: null
+  };
+} 
